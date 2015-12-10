feaa0fa6f3f8e3659d1ed00a23c99501a7353a5c
liusy182 <liusy182@gmail.com>
2015-12-10 15:25:14 +0800

    make use of DBservice directly

--- hackapp/www/js/services/dbService.js
+++ hackapp/www/js/services/dbService.js
@@ -3,75 +3,75 @@ angular.module('starter.services')
 
     return {
     	  //get an object from firebase
-        getAllOrders: function() 
-        {
-        	var ref = new Firebase("https://kopiteh.firebaseio.com/orders");  
-          var returnObj = $firebaseObject(ref);
-          var deferred = $q.defer();
-          try {
-            ref.on("value", function(snapshot) {
-              deferred.resolve({'error':false, 'result': snapshot.val()});
-            }, function (errorObject) {
-              deferred.resolve({'error':true, 'result': errorObject});
-            });
-          }
-          catch (exc) {
-              deferred.resolve({'error':true, 'result': 'exception: ' + exc.toString()});
-          }
-          return deferred.promise;
-        }, 
-        getAllOrdersLength: function() 
-        {
-          var ref = new Firebase("https://kopiteh.firebaseio.com/orders");  
-          var returnObj = $firebaseObject(ref);
-          var deferred = $q.defer();
-          try {
-            ref.on("value", function(snapshot) {
-              deferred.resolve({'error':false, 'result': snapshot.val().length});
-            }, function (errorObject) {
-              deferred.resolve({'error':true, 'result': errorObject});
-            });
-          }
-          catch (exc) {
-              deferred.resolve({'error':true, 'result': 'exception: ' + exc.toString()});
-          }
-          return deferred.promise;
-        }, 
-        getOrderdetail: function(id) 
-        {
+      getAllOrders: function() 
+      {
+      	var ref = new Firebase("https://kopiteh.firebaseio.com/orders");  
+        var returnObj = $firebaseObject(ref);
+        var deferred = $q.defer();
+        try {
+          ref.on("value", function(snapshot) {
+            deferred.resolve({'error':false, 'result': snapshot.val()});
+          }, function (errorObject) {
+            deferred.resolve({'error':true, 'result': errorObject});
+          });
+        }
+        catch (exc) {
+            deferred.resolve({'error':true, 'result': 'exception: ' + exc.toString()});
+        }
+        return deferred.promise;
+      }, 
+      getAllOrdersLength: function() 
+      {
+        var ref = new Firebase("https://kopiteh.firebaseio.com/orders");  
+        var returnObj = $firebaseObject(ref);
+        var deferred = $q.defer();
+        try {
+          ref.on("value", function(snapshot) {
+            deferred.resolve({'error':false, 'result': snapshot.val().length});
+          }, function (errorObject) {
+            deferred.resolve({'error':true, 'result': errorObject});
+          });
+        }
+        catch (exc) {
+            deferred.resolve({'error':true, 'result': 'exception: ' + exc.toString()});
+        }
+        return deferred.promise;
+      }, 
+      getOrderdetail: function(id) 
+      {
 
-          var ref = new Firebase("https://kopiteh.firebaseio.com/orders/" + id + "/Bids");  
-          var returnObj = $firebaseObject(ref);
-          var deferred = $q.defer();
-          try {
-            ref.on("value", function(snapshot) {
-              deferred.resolve({'error':false, 'result': snapshot.val()});
-            }, function (errorObject) {
-              deferred.resolve({'error':true, 'result': errorObject});
-            });
-          }
-          catch (exc) {
-              deferred.resolve({'error':true, 'result': 'exception: ' + exc.toString()});
-          }
-          return deferred.promise;
-        }, 
-       //http post request    
-       setOrder: function(firebaseParams) 
-       {
-          var url = firebaseParams[0];
-          var params = firebaseParams[1];
-          var ref = new Firebase("https://kopiteh.firebaseio.com/" + url);  
-          ref.update(params);            
-       },
-       setBids: function(firebaseParams) 
-       {
-          var oder_id = firebaseParams[0];
-          var bids_id = firebaseParams[1];
-          var params = firebaseParams[2];
-          var completeURL = "https://kopiteh.firebaseio.com/orders/" + oder_id + "/Bids" + "/" + bids_id;
-          var ref = new Firebase(completeURL);  
-          ref.update(params);    
-          //$log.debug(returnObj[Bids].length);
-       },  
+        var ref = new Firebase("https://kopiteh.firebaseio.com/orders/" + id + "/Bids");  
+        var returnObj = $firebaseObject(ref);
+        var deferred = $q.defer();
+        try {
+          ref.on("value", function(snapshot) {
+            deferred.resolve({'error':false, 'result': snapshot.val()});
+          }, function (errorObject) {
+            deferred.resolve({'error':true, 'result': errorObject});
+          });
+        }
+        catch (exc) {
+            deferred.resolve({'error':true, 'result': 'exception: ' + exc.toString()});
+        }
+        return deferred.promise;
+      }, 
+      //http post request    
+      setOrder: function(firebaseParams) 
+      {
+        var url = firebaseParams[0];
+        var params = firebaseParams[1];
+        var ref = new Firebase("https://kopiteh.firebaseio.com/" + url);  
+        ref.update(params);            
+      },
+      setBids: function(firebaseParams) 
+      {
+        var oder_id = firebaseParams[0];
+        var bids_id = firebaseParams[1];
+        var params = firebaseParams[2];
+        var completeURL = "https://kopiteh.firebaseio.com/orders/" + oder_id + "/Bids" + "/" + bids_id;
+        var ref = new Firebase(completeURL);  
+        ref.update(params);    
+        //$log.debug(returnObj[Bids].length);
+      },  
     };
 });
