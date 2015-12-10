22494a112034a929dc278620da9482f787f7be95
Weixi <weixi.chen@autodesk.com>
2015-12-10 18:01:55 +0800

    temp disable badge and notificaion

--- hackapp/www/js/controllers.js
+++ hackapp/www/js/controllers.js
@@ -83,7 +83,6 @@ angular.module('starter.controllers', [])
   $scope.$on('$ionicView.enter', function(e) {
     $scope.showLoading(1500);
     $scope.refreshOrders();
-    $scope.refreshOrders();
   });
 
   $scope.$on('$stateChangeSuccess', function(event, toState) {
@@ -95,55 +94,55 @@ angular.module('starter.controllers', [])
     }
   });
 
-  $ionicPlatform.ready(function () {      
-    $cordovaBadge.promptForPermission();
+  // $ionicPlatform.ready(function () {      
+  //   $cordovaBadge.promptForPermission();
  
-        $scope.setBadge = function(value) {
-            $cordovaBadge.hasPermission().then(function(result) {
-                $cordovaBadge.set(value);
-            }, function(error) {
-                alert(error);
-            });
-        }
-    });
+  //       $scope.setBadge = function(value) {
+  //           $cordovaBadge.hasPermission().then(function(result) {
+  //               $cordovaBadge.set(value);
+  //           }, function(error) {
+  //               alert(error);
+  //           });
+  //       }
+  //   });
 
-    $scope.scheduleSingleNotification = function () {
-      $cordovaLocalNotification.schedule({
-        id: 1,
-        title: 'Warning',
-        text: 'Your order has arrived!',
-        data: {
-          customProperty: 'custom value'
-        }
-      }).then(function (result) {
-        console.log('Notification 1 triggered');
-      });
-    };
+  //   $scope.scheduleSingleNotification = function () {
+  //     $cordovaLocalNotification.schedule({
+  //       id: 1,
+  //       title: 'Warning',
+  //       text: 'Your order has arrived!',
+  //       data: {
+  //         customProperty: 'custom value'
+  //       }
+  //     }).then(function (result) {
+  //       console.log('Notification 1 triggered');
+  //     });
+  //   };
 
-    $scope.scheduleDelayedNotification = function () {
-          var now = new Date().getTime();
-          var _10SecondsFromNow = new Date(now + 10 * 1000);
+  //   $scope.scheduleDelayedNotification = function () {
+  //         var now = new Date().getTime();
+  //         var _10SecondsFromNow = new Date(now + 10 * 1000);
  
-          $cordovaLocalNotification.schedule({
-            id: 2,
-            title: 'Warning',
-            text: 'Im so late',
-            at: _10SecondsFromNow
-          }).then(function (result) {
-            console.log('Notification 2 triggered');
-          });
-    };
+  //         $cordovaLocalNotification.schedule({
+  //           id: 2,
+  //           title: 'Warning',
+  //           text: 'Im so late',
+  //           at: _10SecondsFromNow
+  //         }).then(function (result) {
+  //           console.log('Notification 2 triggered');
+  //         });
+  //   };
 
-  $scope.scheduleEveryMinuteNotification = function () {
-    $cordovaLocalNotification.schedule({
-      id: 3,
-      title: 'Warning',
-      text: 'Dont fall asleep',
-      every: 'minute'
-    }).then(function (result) {
-      console.log('Notification 3 triggered');
-    });
-  };
+  // $scope.scheduleEveryMinuteNotification = function () {
+  //   $cordovaLocalNotification.schedule({
+  //     id: 3,
+  //     title: 'Warning',
+  //     text: 'Dont fall asleep',
+  //     every: 'minute'
+  //   }).then(function (result) {
+  //     console.log('Notification 3 triggered');
+  //   });
+  // };
 
   $scope.refreshOrders = function () {
     dbService.getAllOrders().then(function(data){
